4735903b89eab3ee7c409536d4a63acb16cbddd4
hagan199 <haganemmanuel23@gmail.com>
2023-10-17 09:16:41 +0000

    Update on vue course practice

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,8 +2,24 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
+  methods: {
+    submitForm(){
+
+      alert('Sumbit');
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event) {
+     this.name = event.target.value;
+    },
+  }
 });
 
 app.mount('#events');
